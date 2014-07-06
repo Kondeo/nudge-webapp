b0feafb609984b2c2969610d69bb63863bee2ffb
Julian Poyourow <julianpoyo@gmail.com>
2014-07-06 00:41:32 -0700

    Installed AngularJS

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nudgeWebAppApp
+ */
+angular.module('nudgeWebAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nudgeWebAppApp
+ */
+angular.module('nudgeWebAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
