26a12be8055e6bc4ad2d453138f01c69121b90d8
Julian Poyourow <julianpoyo@gmail.com>
2014-07-09 03:59:12 -0700

    Styled tiles for desktop and mobile with fade

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,40 @@ angular.module('nudgeWebAppApp')
       'Karma'
     ];
 
+    if (document.documentElement.clientWidth > 768) {
+
+        $('.switch1').hover(function() {
+            $('#switch1-1').stop().fadeOut('fast', function() {
+                $('#switch1-2').stop().fadeIn('fast');
+            });        
+        }, function() {
+            $('#switch1-2').stop().fadeOut('fast', function() {
+                $('#switch1-1').stop().fadeIn('fast');
+            });
+        });
+
+        $('.switch2').hover(function() {
+            $('#switch2-1').stop().fadeOut('fast', function() {
+                $('#switch2-2').stop().fadeIn('fast');
+            });        
+        }, function() {
+            $('#switch2-2').stop().fadeOut('fast', function() {
+                $('#switch2-1').stop().fadeIn('fast');
+            });
+        });
+
+        $('.switch3').hover(function() {
+            $('#switch3-1').stop().fadeOut('fast', function() {
+                $('#switch3-2').stop().fadeIn('fast');
+            });        
+        }, function() {
+            $('#switch3-2').stop().fadeOut('fast', function() {
+                $('#switch3-1').stop().fadeIn('fast');
+            });
+        });
+
+    }
+
     $scope.events = Events.query();
 
   });
