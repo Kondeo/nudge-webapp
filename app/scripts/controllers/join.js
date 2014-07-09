cee36ba267829f78096462884c33020f2b284194
octobloc <jacobsirvin@gmail.com>
2014-07-08 18:34:12 -0700

    added javascript for join and login pages so they exist in the webapp

---
+++ app/scripts/controllers/join.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:JoinCtrl
+ * @description
+ * # JoinCtrl
+ * Controller of the nudgeWebAppApp
+ */
+angular.module('nudgeWebAppApp')
+  .controller('JoinCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the nudgeWebAppApp
+ */
+angular.module('nudgeWebAppApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
