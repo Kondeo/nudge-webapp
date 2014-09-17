66df73b175f97e15d2be56cde8c705bc95b35ea9
Julian Poyourow <julianpoyo@gmail.com>
2014-09-17 15:11:45 -0700

    Connected controller to friends page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,7 +39,7 @@ angular
       })
       .when('/friends', {
         templateUrl: 'views/friends.html',
-        //controller: 'AboutCtrl'
+        controller: 'FriendsCtrl'
       })
       .when('/feed', {
         templateUrl: 'views/feed.html',
--- app/scripts/controllers/friends.js
+++ app/scripts/controllers/friends.js
@@ -1,1 +1,24 @@
-//Will hold friends page javascript
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the nudgeWebAppApp
+ */
+angular.module('nudgeWebAppApp')
+  .controller('FriendsCtrl', function ($scope, Login, $location, $rootElement) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+        $location.url(requrl);
+    }
+
+  });
+
