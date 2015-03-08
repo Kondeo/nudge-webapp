452b75e1e2295399f9054dda410726046c9f3580
Julian Poyourow <julianpoyo@gmail.com>
2015-03-08 14:33:21 -0700

    Consolidated getCookie into utilities javascript
    Cleaner, more compact code for getCookie. The utilities javascript can
    now contain any simple utility functions that are used repeatedly.

--- app/scripts/controllers/frienddetails.js
+++ app/scripts/controllers/frienddetails.js
@@ -21,17 +21,4 @@ angular.module('nudgeWebAppApp')
 
     $scope.frienddetails = User.get(submitjson);
 
-    function getCookie(cname) {
-      var name = cname + "=";
-      var ca = document.cookie.split(';');
-      for(var i=0; i<ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0)==' ') c = c.substring(1);
-          if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-      }
-      return "";
-  }
-
-
   });
-
