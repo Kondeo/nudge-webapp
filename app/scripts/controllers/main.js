3fca07dc5545fd1a70390e47f1d771445197fdb8
Julian Poyourow <julianpoyo@gmail.com>
2015-03-08 14:15:38 -0700

    Removed old animation code
    Leftover from when we wanted to animate tiles. Now just junk.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,33 +22,6 @@ angular.module('nudgeWebAppApp')
 
     $scope.token = getCookie("session_token");
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        //alert("You device are mobile is");
-        $('.switch1').delay(1000).animate({ color: '#ffffff' }, 'slow');
-        $('.switch2').delay(1000).animate({ color: '#ffffff' }, 'slow');
-        $('.switch3').delay(1000).animate({ color: '#ffffff' }, 'slow');
-    } else {
-
-        $('.switch1').hover(function() {
-            $('.switch1').animate({ color: '#ffffff' }, 'fast');
-        }, function() {
-            $('.switch1').stop().animate({ color: "#56799c" }, 'fast');
-        });
-
-        $('.switch2').hover(function() {
-            $('.switch2').animate({ color: '#ffffff' }, 'fast');
-        }, function() {
-            $('.switch2').stop().animate({ color: "#56799c" }, 'fast');
-        });
-
-        $('.switch3').hover(function() {
-            $('.switch3').animate({ color: '#ffffff' }, 'fast');
-        }, function() {
-            $('.switch3').stop().animate({ color: "#56799c" }, 'fast');
-        });
-
-    };
-
     if (!(document.cookie.indexOf("session_token") >= 0)) {
         $scope.go("/welcome");
     }
