21bf46933a527f7d4d50a99716a4c550d24dc46d
torch2424 <torch2424@hotmail.com>
2015-03-12 13:29:20 -0700

    Added error catching function to Scope.get

--- app/scripts/controllers/myevents.js
+++ app/scripts/controllers/myevents.js
@@ -18,6 +18,30 @@ angular.module('nudgeWebAppApp')
     $scope.go = function(requrl){
         console.log("url switch for " + requrl);
         $location.url(requrl);
+
+        //To catch Errors when trying to join while the server
+        //Or backend is down
+        function(response){
+            //Status 0 is when the server is not found
+            if(response.status === 0){
+                //Create a string with Response
+                var responseString = "Response is: " + response.status;
+                //Log our response status
+                console.log(responseString);
+                //Now alert it(Under master julian's request)
+                alert("Server is not up! " + responseString);
+            }
+            else
+            {
+                //Show the error to the user
+                 //Create a string with Response
+                var responseString = "Response is: " + response.status;
+                //Log our response status
+                console.log(responseString);
+                //Now alert it(Under master julian's request)
+                alert("There was an error! Error code: " + responseString);
+            }
+        }
     }
 
     var session_token = getCookie("session_token");
