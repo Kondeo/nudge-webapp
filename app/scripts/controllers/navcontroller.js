4ba02102ac9ecbb8a7151e4dc1f11930e12f9401
torch2424 <torch2424@hotmail.com>
2015-03-18 20:27:50 -0700

    added functions and variables to toggle our navar, all working accordingly, but need help with html

--- app/scripts/controllers/navcontroller.js
+++ app/scripts/controllers/navcontroller.js
@@ -6,5 +6,32 @@
  */
 angular.module('nudgeWebAppApp')
   .controller('navController', function ($scope, Events, $location, $rootElement) {
+  	//Function to determine if user is logged in
     $scope.isLoggedIn = isLoggedIn();
+
+    //Function to catch whenever we change our url, works fine
+    $scope.$on('$routeChangeSuccess', function () 
+    {
+    	console.log("changed");
+            setTimeout(function () {
+        $scope.$apply(function(){
+            $scope.toggled = false;
+        });
+    }, 100);
+             console.log($scope.toggled);
+    });
+
+    //Function to change our scope.toggled to true, it works
+    $scope.toggleNav = function()
+    {
+    	console.log("clicked");
+    	 setTimeout(function () {
+        $scope.$apply(function(){
+            $scope.toggled = true;
+        });
+    }, 100);
+
+    	 console.log($scope.toggled);
+    }
+
   });
