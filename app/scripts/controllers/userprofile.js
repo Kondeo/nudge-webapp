747b5c5b549a2832a983dc2414c16660363c6bf8
Julian Poyourow <julianpoyo@gmail.com>
2015-03-21 21:37:20 -0700

    Merge branch user-profiles

---
+++ app/scripts/controllers/userprofile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * JavaScript for friend details
+ * Code retrieves details for a specific friend ID
+ */
+angular.module('nudgeWebAppApp')
+  .controller('UserProfileCtrl', function ($scope, User, Friends, $location, $rootElement, $routeParams) {
+    
+    //Redirects to desired local url
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+        $location.path(requrl);
+    }
+
+    $scope.addFriend = function(){
+        var submitjson = {};
+        submitjson.Id = "add";
+        submitjson.session_token = session_token;
+        submitjson.friend_id = $routeParams.userid;
+        $scope.addResult = Friends.add(submitjson);
+        $scope.userdetails.friend_status = "1";
+    }
+
+    $scope.acceptFriend = function(){
+        var sumbitjson = {};
+        submitjson.Id = "accept";
+        submitjson.session_token = session_token;
+        submitjson.friend_id = $routeParams.userid;
+        $scope.acceptResult = Friends.accept(submitjson);
+        $scope.userdetails.friend_status = "5";
+    }
+
+    $scope.declineFriend = function(){
+        var sumbitjson = {};
+        submitjson.Id = "decline";
+        submitjson.session_token = session_token;
+        submitjson.friend_id = $routeParams.userid;
+        $scope.declineResult = Friends.decline(submitjson);
+        $scope.userdetails.friend_status = "0";
+    }
+
+    //Fetch the session token
+    var session_token = getCookie("session_token");
+    //Define json to be sent to server
+    var submitjson = {};
+    //Add session token and url ID parameter to json
+    submitjson.session_token = session_token;
+    submitjson.Id = $routeParams.userid;
+    //Send json to server and store response in scope variable
+    $scope.userdetails = User.get(submitjson, catchError);
+
+  });
