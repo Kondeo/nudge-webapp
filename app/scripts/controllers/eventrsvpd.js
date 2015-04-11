e2b4fc5a3721fc2e0140d541f9402a9c0d5c0ab7
torch2424 <torch2424@hotmail.com>
2015-04-10 17:53:45 -0700

    Created a function Rsvp'd page

--- app/scripts/controllers/eventrsvpd.js
+++ app/scripts/controllers/eventrsvpd.js
@@ -5,5 +5,20 @@
  * Gets all rsvpd events for the current user from server
  */
 angular.module('nudgeWebAppApp')
-  .controller('EventRsvpdCtrl', function ($scope, MyEvents, $location, $rootElement) {
+  .controller('EventRsvpdCtrl', function ($scope, Events, RSVP, $location, $rootElement) {
+
+  	 //Redirects to desired local url
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+        $location.path(requrl);
+    }
+
+    //Get session token
+    var session_token = getCookie("session_token");
+    //Initialize submission json
+    var submitjson = {};
+    //Add session token to json
+    submitjson.session_token = session_token;
+    //Submit json to server and store result in scope variable
+    $scope.events = RSVP.get(submitjson, catchError);
   });
