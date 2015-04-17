44d182e6d66e2d29f373c09de4b175cf7811a500
torch2424 <torch2424@hotmail.com>
2015-04-17 13:02:20 -0700

    Added unfriends functionality to service

--- app/scripts/services/friends.js
+++ app/scripts/services/friends.js
@@ -3,29 +3,34 @@
 angular.module('nudgeWebAppApp')
   .factory('Friends', ['$resource', function($resource) {
 
-    return $resource( apiBase + 'friend/:Id', 
-        { Id: '@Id' }, { 
-            query: { 
-                method: 'POST', 
-                params: { Id: '' }, 
-                isArray: false 
+    return $resource( apiBase + 'friend/:Id',
+        { Id: '@Id' }, {
+            query: {
+                method: 'POST',
+                params: { Id: '' },
+                isArray: false
             },
-            add: { 
-                method: 'POST', 
-                params: { Id: 'add' }, 
-                isArray: false 
+            add: {
+                method: 'POST',
+                params: { Id: 'add' },
+                isArray: false
             },
-            accept: { 
-                method: 'POST', 
+            remove: {
+                method: 'POST',
+                params: { Id: 'remove' }, 
+                isArray: false
+            },
+            accept: {
+                method: 'POST',
                 params: { Id: 'accept' },
-                isArray: false 
+                isArray: false
             },
             decline: {
                 method: 'POST',
                 params: { Id: 'decline' },
                 isArray: false
             }
-            
+
         } );
 
 }]);
