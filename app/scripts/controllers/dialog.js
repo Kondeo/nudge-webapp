fa6e08e82dc39f97f6acd1b2ddd19f0851c76921
torch2424 <torch2424@hotmail.com>
2015-04-24 12:49:50 -0700

    Added our session token to our dialog

--- app/scripts/controllers/dialog.js
+++ app/scripts/controllers/dialog.js
@@ -7,6 +7,16 @@
 angular.module('nudgeWebAppApp')
   .controller('DialogCtrl', function ($scope, User, Friends, ngDialog, $location, $rootElement, $routeParams) {
 
+    //Fetch the session token
+    var session_token = getCookie("session_token");
+    //Define json to be sent to server
+    var submitjson = {};
+    //Add session token and url ID parameter to json
+    submitjson.session_token = session_token;
+    submitjson.Id = $routeParams.userid;
+    //Send json to server and store response in scope variable
+    $scope.userdetails = User.get(submitjson, catchError);
+
     //function if they choose yes to remove
     $scope.removeFriend = function()
     {
