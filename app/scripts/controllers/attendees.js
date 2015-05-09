ea81eee41b117d6b42ed00e546ec6603653e819b
torch2424 <torch2424@hotmail.com>
2015-05-08 17:25:45 -0700

    Created a skeleton for our controller and our view

--- app/scripts/controllers/attendees.js
+++ app/scripts/controllers/attendees.js
@@ -1,42 +1,30 @@
 'use strict';
 
 /**
- * JavaScript for event attendees page.
- * Code retrieves all attendees for the host.
+ * @ngdoc function
+ * @name nudgeWebAppApp.controller:AttendeesCtrl
+ * @description
+ * # AttendeesCtrl
+ * Controller of the nudgeWebAppApp
  */
 angular.module('nudgeWebAppApp')
-  .controller('FriendsCtrl', function ($scope, Friends, $location, $rootElement, StringSearch) {
+  .controller('AttendeesCtrl', function ($scope, $routeParams, Events) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karmaa'];
 
-    //Redirects to desired local url
-    $scope.go = function(requrl){
-        console.log("url switch for " + requrl);
-        $location.path(requrl);
-    }
 
-    //Set search results to be empty for html elements
-    $scope.search = {"empty":"true"};
 
-    //For html "No results"
-    $scope.searchSubmitted = false;
 
-    $scope.searchSubmit = function(){
-        //Reset
-        $scope.searchSubmitted = false;
-        //Get results from query
-        $scope.search = StringSearch.search({"query":$scope.searchField}, function(){
-            //For html "No Results"
-            $scope.searchSubmitted = true;
-        });
-    }
 
     //Fetch the session token
     var session_token = getCookie("session_token");
     //Define json to be sent to server
     var submitjson = {};
     //Get url parameter for ID, and add it to the json
-    submitjson.Id = $routeParams.eventid;
+    submitjson.event_id = $routeParams.eventid;
     submitjson.session_token = session_token;
     //Send the json data to the server and store the result in scope variable
     $scope.attendees = Events.hosted(submitjson, catchError);
-
   });
