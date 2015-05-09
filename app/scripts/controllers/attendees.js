37f3b4e3cebb6240301475f02552b3312c4d6727
torch2424 <torch2424@hotmail.com>
2015-05-08 17:09:03 -0700

    Removed our ng click for a tile, and initialized our controller

--- app/scripts/controllers/attendees.js
+++ app/scripts/controllers/attendees.js
@@ -1,17 +1,41 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name nudgeWebAppApp.controller:AttendeesCtrl
- * @description
- * # AttendeesCtrl
- * Controller of the nudgeWebAppApp
+ * JavaScript for event attendees page.
+ * Code retrieves all attendees for the host.
  */
 angular.module('nudgeWebAppApp')
-  .controller('AttendeesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('FriendsCtrl', function ($scope, Friends, $location, $rootElement, StringSearch) {
+
+    //Redirects to desired local url
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+        $location.path(requrl);
+    }
+
+    //Set search results to be empty for html elements
+    $scope.search = {"empty":"true"};
+
+    //For html "No results"
+    $scope.searchSubmitted = false;
+
+    $scope.searchSubmit = function(){
+        //Reset
+        $scope.searchSubmitted = false;
+        //Get results from query
+        $scope.search = StringSearch.search({"query":$scope.searchField}, function(){
+            //For html "No Results"
+            $scope.searchSubmitted = true;
+        });
+    }
+
+    //Fetch the session token
+    var session_token = getCookie("session_token");
+    //Define json to be sent to server
+    var submitjson = {};
+    //Add session token to json
+    submitjson.session_token = session_token;
+    //Submit json to server and store result in scope variable
+    $scope.friends = Friends.query(submitjson, catchError);
+
   });
